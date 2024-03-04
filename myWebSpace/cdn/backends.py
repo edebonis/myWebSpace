319d14972c48f52b7937b0aec2ffc58c22efd25f
Esteban De Bonis <edebonis@sagradoalcal.edu.ar>
2024-03-04 20:05:43 -0300

    added bucket name

--- myWebSpace/cdn/backends.py
+++ myWebSpace/cdn/backends.py
@@ -3,7 +3,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = 'static'
+    bucket_name = 'edebonismedia'
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = 'media'
+    bucket_name = 'edebonismedia'
