4992441354ba7bf7e42a4956af7c72d6848f1489
Esteban De Bonis <edebonis@sagradoalcal.edu.ar>
2024-03-04 21:55:26 -0300

    fixed backends.py

--- myWebSpace/cdn/backends.py
+++ myWebSpace/cdn/backends.py
@@ -1,12 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from myWebSpace.settings import STATIC_ROOT, MEDIA_ROOT
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = STATIC_ROOT
+    location = 'static'
     bucket_name = 'edebonismedia'
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = MEDIA_ROOT
+    location = 'media'
     bucket_name = 'edebonismedia'
